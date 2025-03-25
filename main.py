58d20105c1d7b1eaa8655952836f03b117308c85
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-03-25 20:53:33 +0800

    add initial task assignments and TODOs for function implementations

---
+++ main.py
@@ -0,0 +1,14 @@
+# TODO: Gather order details (product, price, quantity) and store in a list.  
+# Assigned to: Keith  
+
+# TODO: Collect customer details and check if they are a senior citizen.  
+# Assigned to: Annie  
+
+# TODO: Compute total cost of items, applying senior citizen discount if needed.  
+# Assigned to: Zyra  
+
+# TODO: Display items, customer name, senior ID (if any), and total amount.  
+# Assigned to: Mikee  
+
+# TODO: Integrate and execute all functions in the main program.  
+# Assigned to: Kalelle  
